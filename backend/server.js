4507ea04dd676bf94a8d5a83057089d7d5602a50
nongnadear <139845148+nadiiand@users.noreply.github.com>
2026-04-23 02:33:37 +0700

    fetch ข้อมูลหน้า Ropa+Details

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import 'dotenv/config'
 
 import authRoutes from './routes/auth.js'
+import adminRoutes from './routes/admin.js'
+import formRoutes from './routes/form.js'
 
 const app = express()
 
@@ -10,6 +12,8 @@ app.use(cors({ origin: 'http://localhost:3000' }))
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/admin', adminRoutes)
+app.use('/api/form', formRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`)
