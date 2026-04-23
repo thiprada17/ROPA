c98110542e5f8db6fb7449638d727938322a39f5
thiprada17 <tiprada2299@gmail.com>
2026-04-23 12:08:05 +0700

    แก้ USer

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -73,7 +73,7 @@ export default function LoginPage() {
 
       if (role === "Admin") {
         router.push("/admin");
-      } else if (role === "Controller") {
+      } else if (role === "User") {
         router.push("/Ropa");
       }
 
